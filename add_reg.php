66777bd29685bf7684ca4b3ef4468021677a1364
FelipeVM00 <dvargas00@uan.edu.co>
2019-11-21 18:53:48 -0500

    Correccion texto

--- add_reg.php
+++ add_reg.php
@@ -1,7 +1,7 @@
 <html>
 <body>
 <?php
-$conexion=mysqli_connect("fdb18.awardspace.net","3128185_base","Admin1234","3128185_base") or die ("Problemas con la conexion");
+$conexion=mysqli_connect("fdb18.awardspace.net","3128185_base","Admin1234","3128185_base") or die ("Problemas con la conexión");
 mysqli_query($conexion,"insert into alumnos (Nombre,Celular,Direccion) values ('$_REQUEST[Nombre]','$_REQUEST[Celular]','$_REQUEST[Direccion]')") or die ("Problemas en el select:".mysqli_error($conexion));
 mysqli_close($conexion);
 echo "Se insertaron correctamente los datos";
